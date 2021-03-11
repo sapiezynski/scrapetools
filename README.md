8b75b116d1a079b34fb0616240bc46e5bc21d9f6
sapiezynski <sapiezynski@gmail.com>
2021-03-11 00:10:03 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# scrapetools
+`scrapetools` can be used to reverse engineer and modify internal API calls on webpages. 
+
+## usage
+* Start by opening the Developer Tools in your browser and indentifying which call you might want to replicate.
+* Right click on that call and select "Copy -> Copy as cURL"
+* paste the clipboard as a string in your notebook (with double quotation marks)
+	* for example I copied a call from AllRecipes
+	
+	```
+	curl_str = "curl 'https://www.allrecipes.com/element-api/content-proxy/faceted-searches-load-more?search=spinach&page=2' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:86.0) Gecko/20100101 Firefox/86.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=1' --compressed -H 'X-Requested-With: XMLHttpRequest' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: https://www.allrecipes.com/search/results/?search=spinach' -H 'Cookie: removed for privacy reasons' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: Trailers'"
+	```
+* transform this string into something more usable:
+
+```
+import scrapetools
+uncurled = scrapetools.uncurl(curl_str)
+```
+
+* now `uncurled` is a dictionary:
+
+```
+{
+  "base_url": "https://www.allrecipes.com/element-api/content-proxy/faceted-searches-load-more",
+  "params": {
+    "search": "spinach",
+    "page": "2"
+  },
+  "headers": {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:86.0) Gecko/20100101 Firefox/86.0",
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=1",
+    "X-Requested-With": "XMLHttpRequest",
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Referer": "https://www.allrecipes.com/search/results/?search=spinach",
+    "Cookie": "removed for privacy reasons",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache",
+    "TE": "Trailers"
+  }
+} 
+```
+* we can clearly see that the search term is under `uncurled['params']['search']` and the page number is under `uncurled['params']['page']`
+* we can modify either of these values and search for another ingredient or a different page:
+
+```
+uncurled['page'] = 3
+```
+* after making the necessary adjustment, we can make the modified request:
+
+```
+response = scrapetools.request(uncurled)
+```
---
+++ scrapetools.py
@@ -0,0 +1,101 @@
+import requests
+import re
+
+
+def extract_header(header_str):
+    """
+    given a string in the format of '-H \''key: value\'' it returns a dictionary with
+    the corresponding key and value
+    
+    Args:
+        header_str (str): string containing a header in the curl format
+
+    Returns:
+        header_dict (dict): a dictionary representation of that header
+
+    """
+
+    header_parts = header_str.split('-H ')[1].replace('\'','').split(': ')
+    key = header_parts[0]
+    value = ': '.join(header_parts[1:])
+    return {key: value}
+
+def extract_headers(curl_str):
+    """
+    given a whole result of Copy as cURL it extract all the headers and 
+    represents them as a dictionary
+
+    Args:
+        curl_str (str): string as copied from the browser developer tools
+
+    Returns:
+        headers_dict (dict): a dictionary of all headers
+    """
+
+    pattern = '-H \'[^\']+\''
+    headers = re.findall(pattern, curl_str)
+    headers_dict = {}
+    for header in headers:
+        headers_dict.update(extract_header(header))
+    return headers_dict
+
+def parse_url(url):
+    """
+    given a whole url, breaks it down to base url and a dictionary of get parameters
+
+    Args:
+        url (str): a whole url
+
+    Returns:
+        url_dict (dict): a dictionary containing the 'base_url' as well as a dictionary of 'params'
+    """
+
+    unquoted = requests.utils.unquote(url)
+    url_parts = unquoted.split('?')
+    base = url_parts[0]
+    
+    if len(url_parts) == 1:
+        return {'base_url':base, 'params':{}}
+    
+    params = url_parts[1]
+    params_dict = {}
+    
+    for param in params.split('&'):
+        param_parts = param.split('=')
+        key = param_parts[0]
+        value = '='.join(param_parts[1:])
+        params_dict[key] = value
+        
+    return {'base_url':base, 'params':params_dict}
+
+def uncurl(curl_str):
+    """
+    given the whole Copy as cURL string, extracts the url and get params as well as headers
+
+    Args:
+        curl_str (str): Copy as cURL string from the dev tools
+
+    Returns:
+        uncurled (dict): a dictionary representation of the cURL command
+    """
+
+    url = curl_str.split(' ')[1].replace('\'','')
+    uncurled = {}
+    uncurled.update(parse_url(url))
+    uncurled['headers'] = extract_headers(curl_str)
+    return uncurled
+
+def request(uncurled):
+    """
+    given the uncurled request from the uncurl function, it makes a get request with the url and headers
+
+    Args:
+        uncurled (dict): the result of the uncurl function
+
+    Returns:
+        response (requests.Response): the result of the request
+    """
+    params = '&'.join([f'{key}={value}' for key, value in uncurled['params'].items()])
+    url = f'{uncurled["base_url"]}?{params}'
+    return requests.get(url, headers=uncurled['headers'])
+
